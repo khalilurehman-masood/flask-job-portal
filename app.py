c2063c8791c004b82586ed5da6aa4fe10634214d
khalilurehman-masood <82248271+khalilurehman-masood@users.noreply.github.com>
2024-08-03 02:33:28 +0000

    Added dynamic information to the site

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {'id':1,
+  'title':'Data Analyst',
+   'location':'Islamabad',
+  'salary':'Rs.10,00,000'},
+  {'id':2,
+    'title':'Data Scientist',
+     'location':'Karachi'}
+]
+
 #app.route decorator,with '/' as the argument
 @app.route("/")
-def hello_world():
-  return 'hello world'
+def jobs():
+  return render_template('home.html',jobs=JOBS)
+
+#api for the jobs
+@app.route("/api/jobs")
+def api_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0',debug = True)
